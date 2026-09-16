01d6eccd54e9d10176076ada741a595070b967b0
Gustavo Moreno <330175480+gmorenos-dev@users.noreply.github.com>
2026-09-16 20:38:39 -0300

    chore: initialize financial api

---
+++ src/test/java/com/gmoreno/financialapi/FinancialApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gmoreno.financialapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinancialApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
