7203517a1e99c6c8b8b260fb5c229cffa30d46ff
Gustavo Moreno <330175480+gmorenos-dev@users.noreply.github.com>
2026-09-17 15:42:09 -0300

    feat: add transaction model

---
+++ src/main/java/com/gmoreno/financialapi/model/Transaction.java
@@ -0,0 +1,55 @@
+package com.gmoreno.financialapi.model;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+public class Transaction {
+
+    private Long id;
+    private String description;
+    private BigDecimal amount;
+    private String type;
+    private LocalDate date;
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public BigDecimal getAmount() {
+        return amount;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setAmount(BigDecimal amount) {
+        this.amount = amount;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+}
+
